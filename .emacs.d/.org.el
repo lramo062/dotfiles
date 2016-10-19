db371fb44d64fc7a97d85ab30f0e990ba6dd5296
Lester Ramos <lramo062@fiu.edu>
2016-10-19 12:31:14 -0400

    Update .org.el

--- .emacs.d/.org.el
+++ .emacs.d/.org.el
@@ -3,9 +3,9 @@
 (require 'org)
 (require 'org-gcal)
 
-(setq org-gcal-client-id "1093351195296-r2f77gdi4pngo84q5mefc0sm20d42qhh.apps.googleusercontent.com"
-      org-gcal-client-secret "9dsu4vE99jHZl5tsTXOozGF9"
-      org-gcal-file-alist '(("lramo0729@gmail.com" .  "~/.emacs.d/calendar.org")))
+(setq org-gcal-client-id "**********"
+      org-gcal-client-secret "*********"
+      org-gcal-file-alist '(("**********" .  "~/.emacs.d/calendar.org")))
 
 ;; Org-Agenda
 (setq org-agenda-files (list "~/.emacs.d/agenda.org"))
