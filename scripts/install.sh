4c4bcdd5828f77e661a6f31e89777efce20dea9e
Lester Ramos <lramo062@fiu.edu>
2018-12-27 19:21:41 -0500

    updated package installs

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+sudo pacman -S zsh emacs bspwm sxhkd scrot ttf-hack i3lock termite 
+dmenu feh git blueman compton docker ffmpeg firefox nautilus ranger 
+tmux --no-confirm
+
+yaourt -S polybar
+
+git clone https://github.com/lramo062/dotfiles
+
