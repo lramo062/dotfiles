659a0dcf16c4889bd0edb8d68cd8ca1b2fe743ca
Lester Ramos <lramo062@fiu.edu>
2018-12-29 18:46:44 -0500

    added scripting setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,34 @@
-sudo pacman -S zsh emacs bspwm sxhkd scrot ttf-hack i3lock termite 
-dmenu feh git blueman compton docker ffmpeg firefox nautilus ranger 
-tmux --no-confirm
+#!/bin/sh
 
-yaourt -S polybar
+# update system
+sudo pacman -Syu
 
+# pacman packages 
+sudo pacman -S yaourt chromium zsh emacs bspwm sxhkd
+scrot ttf-hack ttf-monaco noto-fonts i3lock termite 
+dmenu feh git blueman compton docker ffmpeg
+firefox nautilus ranger tmux arc-gtk-theme --no-confirm
+
+# yaourt packages
+yaourt -S polybar paper-icon-theme-git
+oh-my-zsh-git ttf-font-awesome
+ttf-all-the-icons wal-git --noconfirm
+
+# clone dotfiles
 git clone https://github.com/lramo062/dotfiles
 
+# emacs
+cp -r dotfiles/new_emacs_config/.emacs.d ~/
+cp dotfiles/new_emacs_config/.emacs ~/
+
+# create config folders and add files to them
+cp -r dotfiles/bspwm .config/
+cp -r dotfiles/polybar .config/
+cp -r dotfiles/sxhkd .config/
+cp -r dotfiles/termite .config/
+cp -r dotfiles/polybar .config/
+cp -r dotfiles/compton .config/
+
+# zsh setup
+sudo chsh -s /usr/bin/zsh lester
+cp /usr/share/oh-my-zsh/zshrc ~/.zshrc
